5eb44a327da943288cbde011465f98a7741e669e
Yash Pandey <ypandey7730@gmail.com>
2025-10-11 11:32:41 +0000

    more smaller

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <Image src="/LOGO.png" alt="Phone Point Logo" width={100} height={28} />
+      <Image src="/LOGO.png" alt="Phone Point Logo" width={80} height={22} />
     </Link>
   );
 }

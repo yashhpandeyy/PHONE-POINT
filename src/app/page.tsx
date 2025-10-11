396fea88e8bf15d42684f6b2f356f605613548e1
Yash Pandey <ypandey7730@gmail.com>
2025-10-11 15:04:18 +0000

    can you make photo small and fixed for web page its currently going outs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function WelcomePage() {
   return (
     <div
-      className="relative flex h-screen w-full flex-col items-center justify-end bg-cover bg-center"
+      className="relative flex h-screen w-full flex-col items-center justify-end bg-contain bg-no-repeat bg-center"
       style={{ backgroundImage: "url('/phonepoint.png')" }}
     >
       <div className="absolute inset-0 bg-black/30" />
